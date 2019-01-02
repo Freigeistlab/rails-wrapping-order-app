e0348b4ec27d21e74d724a1671e8eda6d8810032
David Reissner <d.reissner@gmail.com>
2019-01-02 16:50:30 +0100

    Create Order Status

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :order_statuses
   resources :products
   resources :product_groups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
