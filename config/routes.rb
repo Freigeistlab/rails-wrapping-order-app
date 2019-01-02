066df9f1182b87149441acd2064113076ae8a11e
David Reissner <d.reissner@gmail.com>
2019-01-02 17:24:33 +0100

    Add home route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :order_statuses
   resources :products
   resources :product_groups
+
+  root to: "products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
